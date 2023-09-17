69a5904cc3bda1586afe386b25aef34963b7db76
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-09-18 02:33:01 +0530

    fixes some issues
    the navigation dot module have some issues ( not responding on the 4th section ). fixes this bug.
    
    Date: 18 September 2023

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
               >
                 <HiX onClick={() => setToggle(false)} />
                 <ul>
-                  {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                  {['home', 'about', 'work', 'testimonial', 'skills', 'contact'].map((item) => (
                       <li key={item}>
                         <a href={`#${item}`} onClick={() => setToggle(false)}>
                           {item}
--- frontend/src/components/NavigationDots.jsx
+++ frontend/src/components/NavigationDots.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavigationDots = ({ active }) => {
   return (
     <div className='app__navigation'>
-        {['home', 'about', 'work', 'testimonials', 'skills', 'contact'].map((item, index) => (
+        {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
             <a 
                 href={`#${item}`} 
                 key={item + index}
@@ -15,4 +15,4 @@ const NavigationDots = ({ active }) => {
   )
 }
 
-export default NavigationDots
+export default NavigationDots;
