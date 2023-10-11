b1c08a6b805b0d986b7cb567e18e0a25fbc1deec
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-11 15:17:07 +0530

    Added resume page
    added resume page on the main project. Exported it using index.js file, added it in the navbar and app.js page, then added it in the navigationdots page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { About, Footer, Header, Skills, Resume, Testimonial, Work } from './container';
 import { Navbar } from './components';
 
 import './App.scss';
@@ -13,7 +13,8 @@ const App = () => {
       <About />
       <Work />
       <Skills />
-      <Testimonial />
+      <Resume />
+      {/* <Testimonial /> */}
       <Footer />
     </div>
   );
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
               >
                 <HiX onClick={() => setToggle(false)} />
                 <ul>
-                  {['home', 'about', 'work', 'testimonial', 'skills', 'contact'].map((item) => (
+                  {['home', 'about', 'work', 'resume', 'skills', 'contact'].map((item) => (
                       <li key={item}>
                         <a href={`#${item}`} onClick={() => setToggle(false)}>
                           {item}
